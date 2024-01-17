d1f3df561e3b14ea23552a818b8b2d8d29d1c8a5
GamingYoung <zzzz7057@163.com>
2024-01-17 14:31:53 +0800

    127 unfinished

--- 127. 单词接龙.js
+++ 127. 单词接龙.js
@@ -7,6 +7,7 @@
 var ladderLength = function(beginWord, endWord, wordList) {
     wordList.push(beginWord);
     wordList.push(endWord);
+    const neibor = new Array(wordList.length).fill(0).map(() => new Array(wordList.length).fill(0));
     for (let i = 0; i < wordList.length; i++) {
         
     }
