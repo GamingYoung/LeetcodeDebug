56f37bb154589a5125393132c418380cd3df8b5b
GamingYoung <zzzz7057@163.com>
2024-01-23 18:01:07 +0800

    bfs, dfs

---
+++ 1971. 寻找图中是否存在路径.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    const adj = new Array(n).fill(0).map(() => new Array());
+    for (const edge of edges) {
+        const x = edge[0], y = edge[1];
+        adj[x].push(y);
+        adj[y].push(x);
+    }
+    const visited = new Array(n).fill(false);
+    const queue = [source];
+    visited[source] = true;
+    while (queue.length) {
+        const vertex = queue.shift();
+        if (vertex === destination) {
+            break;
+        }
+        for (const next of adj[vertex]) {
+            if (!visited[next]) {
+                queue.push(next);
+                visited[next] = true;
+            }
+        }
+    }
+    return visited[destination];
+
+};
+
+validPath(6, [[0,1],[0,2],[3,5],[5,4],[4,3]], 0, 5)
