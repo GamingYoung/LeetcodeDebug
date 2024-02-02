09a6ac8ea5aca86c0823a67fee347cdb50356058
GamingYoung <zzzz7057@163.com>
2024-02-02 12:19:08 +0800

    165finished

--- 165. 比较版本号.js
+++ 165. 比较版本号.js
@@ -6,7 +6,24 @@
 var compareVersion = function(version1, version2) {
     const arr1 = version1.split('.');
     const arr2 = version2.split('.');
-    return arr1;
+    let temp = 0;
+    while (temp < arr1.length && temp < arr2.length) {
+        if (Number(arr1[temp]) < Number(arr2[temp])) return -1;
+        else if (Number(arr1[temp]) > Number(arr2[temp])) return 1;
+        else temp += 1;
+    }
+    if (arr1.length > arr2.length) {
+        for (let i = arr2.length; i < arr1.length; i++) {
+            if (Number(arr1[i]) != 0) return 1;
+        }
+        return 0;
+    } else if (arr1.length < arr2.length) {
+        for (let i = arr1.length; i < arr2.length; i++) {
+            if (Number(arr2[i]) != 0) return -1;
+        }
+        return 0;
+    }
+    else return 0;
 };
 
 compareVersion("1.01", "1.001");
