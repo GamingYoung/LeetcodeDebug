9ce2b90f8fc1315eb3929e21b0fffac5c374657d
GamingYoung <zzzz7057@163.com>
2024-02-02 11:43:34 +0800

    finished

---
+++ 165. 比较版本号.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    const arr1 = version1.split('.');
+    const arr2 = version2.split('.');
+    return arr1;
+};
+
+compareVersion("1.01", "1.001");
