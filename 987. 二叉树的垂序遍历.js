83a2b2043d8a0db1687731294bd991ad30b4d702
GamingYoung <zzzz7057@163.com>
2024-02-13 10:44:06 +0800

    1asy

---
+++ 987. 二叉树的垂序遍历.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var verticalTraversal = function(root) {
+    const cur = [];
+    const inOrder = function(node, row, col) {
+        cur.push([node.val, row, col]);
+        if (node.left) inOrder(node.left, row + 1, col - 1);
+        if (node.right) inOrder(node.right, row + 1, col + 1);
+    }
+    inOrder(root, 0, 0);
+    cur.sort((a, b) => {
+        if (a[2] === b[2]) {
+            if (a[1] === b[1]) {
+                return a[0] - b[0];
+            } else {
+                return a[1] - b[1];
+            }
+        } else {
+            return a[2] - b[2];
+        }
+    })
+    const res = [];
+    let path = [cur[0][0]];
+    for (let i = 1; i < cur.length; i++) {
+        if (cur[i][2] === cur[i - 1][2]) path.push(cur[i][0]);
+        else {
+            res.push([...path]);
+            path = [cur[i][0]];
+        }
+    }
+    res.push(...[path]);
+    return res;
+};
